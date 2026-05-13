a03b7b5160685932a6c43578c02617d3ae0ad605
KaloyanHristovDimov <KaloyanHristovDimov@gmail.com>
2026-05-13 15:52:59 +0200

    Remove door generation

--- DungeonGeneration/Assets/Scripts/DungeonGeneratorScript.cs
+++ DungeonGeneration/Assets/Scripts/DungeonGeneratorScript.cs
@@ -6,19 +6,19 @@ using UnityEngine.LightTransport;
 
 public class DungeonGeneratorScript : MonoBehaviour
 {
-    public int wallSize = 2;
-    public int minRoomSize = 10;
+    public int wallSize = 1;
+    public int minRoomSize = 5;
     public RectInt startRoomParams = new RectInt(0, 0, 100, 100);
     List<RectInt> roomsToDraw = new List<RectInt>();
     List<RectInt> newRooms = new List<RectInt>();
     List<RectInt> roomsToRemove = new List<RectInt>();
-    List<RectInt> DoorsToDraw = new List<RectInt>();
+    //List<RectInt> DoorsToDraw = new List<RectInt>();
     bool keepDividing = true;
     public bool wait = false;
     public float randomSizeMin = 0.05f;
-    public float randomSizeMax = 0.20f;
+    public float randomSizeMax = 0.75f;
     public int wallHeight = 3;
-    public int doorSize = 1;
+    //public int doorSize = 1;
 
     void Start()
     {
@@ -29,7 +29,7 @@ public class DungeonGeneratorScript : MonoBehaviour
             keepDividing = false;
             foreach (var room in roomsToDraw)
             {
-                if (TrySplit(room, out RectInt roomA, out RectInt roomB)) 
+                if (TrySplit(room, out RectInt roomA, out RectInt roomB))
                 {
                     roomsToRemove.Add(room);
                     newRooms.Add(roomA);
@@ -44,44 +44,44 @@ public class DungeonGeneratorScript : MonoBehaviour
             newRooms.Clear();
         }
         AlgorithmsUtils.DebugRectInt(baseRoom, Color.yellow, 1000f, false, wallHeight);
-        FixDoorsAfterSplits();
+        //FixDoorsAfterSplits();
         if (wait) StartCoroutine(DrawRooms());
-        else 
+        else
         {
             foreach (var r in roomsToDraw)
             {
                 AlgorithmsUtils.DebugRectInt(r, Color.yellow, 1000f, false, wallHeight);
             }
-            DrawDoorsI();
+            //DrawDoorsI();
         }
     }
 
-    private void DrawDoorsI()
-    {
-        foreach (var r in DoorsToDraw)
-        {
-            AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
-        }
-    }
+    //private void DrawDoorsI()
+    //{
+    //    foreach (var r in DoorsToDraw)
+    //    {
+    //        AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
+    //    }
+    //}
 
-    IEnumerator DrawRooms() 
+    IEnumerator DrawRooms()
     {
         foreach (var r in roomsToDraw)
         {
             AlgorithmsUtils.DebugRectInt(r, Color.yellow, 1000f, false, wallHeight);
             yield return new WaitForSeconds(0.1f);
         }
-        StartCoroutine(DrawDoors());
+        //StartCoroutine(DrawDoors());
     }
 
-    IEnumerator DrawDoors()
-    {
-        foreach (var r in DoorsToDraw)
-        {
-            AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
+    //IEnumerator DrawDoors()
+    //{
+    //    foreach (var r in DoorsToDraw)
+    //    {
+    //        AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
+    //        yield return new WaitForSeconds(0.1f);
+    //    }
+    //}
 
     private bool TrySplit(RectInt room, out RectInt roomA, out RectInt roomB)
     {
@@ -110,9 +110,6 @@ public class DungeonGeneratorScript : MonoBehaviour
             roomA = new RectInt(room.x, room.y, aWidth, room.height);
             roomB = new RectInt(room.xMax - bWidth, room.y, bWidth, room.height); // CHANGED (centered wall)
 
-            int doorY = UnityEngine.Random.Range(room.yMin, room.yMax - doorSize);
-            RectInt door = new RectInt(roomA.xMax, doorY, wallSize, doorSize);
-            DoorsToDraw.Add(door);
             return true;
         }
         else
@@ -137,130 +134,127 @@ public class DungeonGeneratorScript : MonoBehaviour
             roomA = new RectInt(room.x, room.y, room.width, aHeight);
             roomB = new RectInt(room.x, room.yMax - bHeight, room.width, bHeight); // CHANGED (centered wall)
 
-            int doorX = UnityEngine.Random.Range(room.xMin, room.xMax - doorSize);
-            RectInt door = new RectInt(doorX, roomA.yMax, doorSize, wallSize);
-            DoorsToDraw.Add(door);
             return true;
         }
     }
 
-    public bool IsSplittable(RectInt room, bool splitDirection) 
+    public bool IsSplittable(RectInt room, bool splitDirection)
     {
         if (splitDirection)
-            if (room.height >= minRoomSize*2 + wallSize) return true; else return false;
-            else if (room.width >= minRoomSize*2 + wallSize) return true; else return false;
+            if (room.height >= minRoomSize * 2 + wallSize) return true; else return false;
+        else if (room.width >= minRoomSize * 2 + wallSize) return true; else return false;
     }
 
-    public bool RandomBool() 
+    public bool RandomBool()
     {
         int number = UnityEngine.Random.Range(0, 10000);
-        if (number %2 == 0) return true;
+        if (number % 2 == 0) return true;
         else return false;
     }
 
-    private bool HasRoomOnBothSides(RectInt door)
-    {
-        bool isVerticalDoor = door.width == wallSize;   // X-split door: spans wall in X, slides in Y
-        bool isHorizontalDoor = door.height == wallSize; // Y-split door: spans wall in Y, slides in X
+    //private bool HasRoomOnBothSides(RectInt door)
+    //{
+    //    bool isVerticalDoor = door.width == wallSize;   // X-split door: spans wall in X, slides in Y
+    //    bool isHorizontalDoor = door.height == wallSize; // Y-split door: spans wall in Y, slides in X
 
-        if (isVerticalDoor)
-        {
-            // Door occupies the wall gap at x = door.x .. door.x+wallSize
-            // Room on left should touch door.x, room on right should touch door.x+wallSize
-            int leftTouchX = door.x;
-            int rightTouchX = door.x + wallSize;
+    //    if (isVerticalDoor)
+    //    {
+    //        // Door occupies the wall gap at x = door.x .. door.x+wallSize
+    //        // Room on left should touch door.x, room on right should touch door.x+wallSize
+    //        int leftTouchX = door.x;
+    //        int rightTouchX = door.x + wallSize;
 
-            bool hasLeft = false, hasRight = false;
+    //        bool hasLeft = false, hasRight = false;
 
-            for (int i = 0; i < roomsToDraw.Count; i++)
-            {
-                RectInt r = roomsToDraw[i];
+    //        for (int i = 0; i < roomsToDraw.Count; i++)
+    //        {
+    //            RectInt r = roomsToDraw[i];
 
-                // Overlap test on Y between room and door opening
-                bool overlapY = r.yMin < door.yMax && r.yMax > door.yMin;
-                if (!overlapY) continue;
+    //            // Overlap test on Y between room and door opening
+    //            bool overlapY = r.yMin < door.yMax && r.yMax > door.yMin;
+    //            if (!overlapY) continue;
 
-                if (r.xMax == leftTouchX) hasLeft = true;
-                if (r.xMin == rightTouchX) hasRight = true;
+    //            if (r.xMax == leftTouchX) hasLeft = true;
+    //            if (r.xMin == rightTouchX) hasRight = true;
 
-                if (hasLeft && hasRight) return true;
-            }
+    //            if (hasLeft && hasRight) return true;
+    //        }
 
-            return false;
-        }
-        else if (isHorizontalDoor)
-        {
-            // Door occupies wall gap at y = door.y .. door.y+wallSize
-            int bottomTouchY = door.y;
-            int topTouchY = door.y + wallSize;
+    //        return false;
+    //    }
+    //    else if (isHorizontalDoor)
+    //    {
+    //        // Door occupies wall gap at y = door.y .. door.y+wallSize
+    //        int bottomTouchY = door.y;
+    //        int topTouchY = door.y + wallSize;
 
-            bool hasBottom = false, hasTop = false;
+    //        bool hasBottom = false, hasTop = false;
 
-            for (int i = 0; i < roomsToDraw.Count; i++)
-            {
-                RectInt r = roomsToDraw[i];
+    //        for (int i = 0; i < roomsToDraw.Count; i++)
+    //        {
+    //            RectInt r = roomsToDraw[i];
 
-                bool overlapX = r.xMin < door.xMax && r.xMax > door.xMin;
-                if (!overlapX) continue;
+    //            bool overlapX = r.xMin < door.xMax && r.xMax > door.xMin;
+    //            if (!overlapX) continue;
 
-                if (r.yMax == bottomTouchY) hasBottom = true;
-                if (r.yMin == topTouchY) hasTop = true;
+    //            if (r.yMax == bottomTouchY) hasBottom = true;
+    //            if (r.yMin == topTouchY) hasTop = true;
 
-                if (hasBottom && hasTop) return true;
-            }
+    //            if (hasBottom && hasTop) return true;
+    //        }
 
-            return false;
-        }
+    //        return false;
+    //    }
 
-        // Unknown door orientation -> treat as invalid
-        return false;
-    }
+    //    // Unknown door orientation -> treat as invalid
+    //    return false;
+    //}
 
-    private void FixDoorsAfterSplits()
-    {
-        for (int i = DoorsToDraw.Count - 1; i >= 0; i--)
-        {
-            RectInt d = DoorsToDraw[i];
+    //private void FixDoorsAfterSplits()
+    //{
+    //    for (int i = DoorsToDraw.Count - 1; i >= 0; i--)
+    //    {
+    //        RectInt d = DoorsToDraw[i];
 
-            if (HasRoomOnBothSides(d))
-                continue;
+    //        if (HasRoomOnBothSides(d))
+    //            continue;
 
-            // Try nudging along the "sliding" axis
-            RectInt dPlus = d;
-            RectInt dMinus = d;
+    //        // Try nudging along the "sliding" axis
+    //        RectInt dPlus = d;
+    //        RectInt dMinus = d;
 
-            if (d.width == wallSize)
-            {
-                // slides along Y
-                dPlus.y += doorSize;
-                dMinus.y -= doorSize;
-            }
-            else if (d.height == wallSize)
-            {
-                // slides along X
-                dPlus.x += doorSize;
-                dMinus.x -= doorSize;
-            }
-            else
-            {
-                DoorsToDraw.RemoveAt(i);
-                continue;
-            }
+    //        if (d.width == wallSize)
+    //        {
+    //            // slides along Y
+    //            dPlus.y += doorSize;
+    //            dMinus.y -= doorSize;
+    //        }
+    //        else if (d.height == wallSize)
+    //        {
+    //            // slides along X
+    //            dPlus.x += doorSize;
+    //            dMinus.x -= doorSize;
+    //        }
+    //        else
+    //        {
+    //            DoorsToDraw.RemoveAt(i);
+    //            continue;
+    //        }
 
-            if (HasRoomOnBothSides(dPlus))
-            {
-                DoorsToDraw[i] = dPlus;
-                continue;
-            }
+    //        if (HasRoomOnBothSides(dPlus))
+    //        {
+    //            DoorsToDraw[i] = dPlus;
+    //            continue;
+    //        }
 
-            if (HasRoomOnBothSides(dMinus))
-            {
-                DoorsToDraw[i] = dMinus;
-                continue;
-            }
+    //        if (HasRoomOnBothSides(dMinus))
+    //        {
+    //            DoorsToDraw[i] = dMinus;
+    //            continue;
+    //        }
 
-            // Still bad -> delete it
-            DoorsToDraw.RemoveAt(i);
-        }
-    }
+    //        // Still bad -> delete it
+    //        DoorsToDraw.RemoveAt(i);
+    //    }
+    //}
 }
