906bb5c121f6451847675afb7a7b276d8160bc8c
KaloyanHristovDimov <KaloyanHristovDimov@gmail.com>
2026-05-15 15:42:42 +0200

    Fix access modifiers and clean up code

--- DungeonGeneration/Assets/Scripts/DungeonGeneratorScript.cs
+++ DungeonGeneration/Assets/Scripts/DungeonGeneratorScript.cs
@@ -9,28 +9,25 @@ using UnityEngine.LightTransport;
 
 public class DungeonGeneratorScript : MonoBehaviour
 {
-    public int minRoomSize = 5;
-    public RectInt startRoomParams = new RectInt(0, 0, 100, 100);
-    List<RectInt> roomsToDraw = new List<RectInt>();
-    List<RectInt> newRooms = new List<RectInt>();
-    List<RectInt> roomsToRemove = new List<RectInt>();
-    //List<RectInt> DoorsToDraw = new List<RectInt>();
-    bool keepDividing = true;
-    public bool wait = false;
-    public float randomSizeMin = 0.05f;
-    public float randomSizeMax = 0.75f;
-    public int wallHeight = 3;
-    public bool spawnAssets = false;
-    public GameObject wallPrefab;
-    List<Vector3> takenPositions = new List<Vector3>();
-    public GameObject floorPrefab;
-    public GameObject cellingPrefab;
-    //public int doorSize = 1;
+    [SerializeField] private int minRoomSize = 5;
+    [SerializeField] private RectInt startRoomParams = new RectInt(0, 0, 100, 100);
+    [SerializeField] private bool wait = false;
+    [SerializeField] private float randomSizeMin = 0.05f;
+    [SerializeField] private float randomSizeMax = 0.75f;
+    [SerializeField] private int wallHeight = 3;
+    [SerializeField] private bool spawnAssets = false;
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] private GameObject floorPrefab;
+    [SerializeField] private GameObject cellingPrefab;
+    private List<RectInt> roomsToDraw = new List<RectInt>();
+    private List<RectInt> newRooms = new List<RectInt>();
+    private List<RectInt> roomsToRemove = new List<RectInt>();
+    private bool keepDividing = true;
+    private List<Vector3> takenPositions = new List<Vector3>();
+    private List<RectInt> Doors = new List<RectInt>();
 
     void Start()
     {
-
-        //DebugDrawingBatcher.GetInstance().ClearAllBatchedCalls();
         GenerateDungeon();
     }
 
@@ -61,8 +58,6 @@ public class DungeonGeneratorScript : MonoBehaviour
             foreach (var room in newRooms) roomsToDraw.Add(room);
             newRooms.Clear();
         }
-        //AlgorithmsUtils.DebugRectInt(roomsToDraw[0], Color.yellow, 1000f, false, wallHeight);
-        //FixDoorsAfterSplits();
         if (wait) StartCoroutine(DrawRooms());
         else
         {
@@ -70,14 +65,13 @@ public class DungeonGeneratorScript : MonoBehaviour
             {
                 DebugDrawingBatcher.GetInstance().BatchCall(() =>
                 {
-                    AlgorithmsUtils.DebugRectInt(r, Color.yellow/*, 1000f, false, wallHeight*/);
+                    AlgorithmsUtils.DebugRectInt(r, Color.yellow);
                 });
             }
             if (spawnAssets)
             {
                 SpawnAssets();
             }
-            //DrawDoorsI();
         }
     }
 
@@ -144,35 +138,17 @@ public class DungeonGeneratorScript : MonoBehaviour
         }
     }
 
-    //private void DrawDoorsI()
-    //{
-    //    foreach (var r in DoorsToDraw)
-    //    {
-    //        AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
-    //    }
-    //}
-
     IEnumerator DrawRooms()
     {
         foreach (var r in roomsToDraw)
         {
             DebugDrawingBatcher.GetInstance().BatchCall(() =>
             {
-                AlgorithmsUtils.DebugRectInt(r, Color.yellow/*, 1000f, false, wallHeight*/);
+                AlgorithmsUtils.DebugRectInt(r, Color.yellow);
             });
             yield return new WaitForSeconds(0.1f);
         }
-        //StartCoroutine(DrawDoors());
     }
-
-    //IEnumerator DrawDoors()
-    //{
-    //    foreach (var r in DoorsToDraw)
-    //    {
-    //        AlgorithmsUtils.DebugRectInt(r, Color.red, 1000f, false, wallHeight);
-    //        yield return new WaitForSeconds(0.1f);
-    //    }
-    //}
 
     private bool TrySplit(RectInt room, out RectInt roomA, out RectInt roomB)
     {
@@ -184,22 +160,22 @@ public class DungeonGeneratorScript : MonoBehaviour
             // X
             if (room.width < minRoomSize * 2) return false;
 
-            int usable = room.width;                 // CHANGED: total room space after wall
-            float minRatio = (float)minRoomSize / usable;       // CHANGED
-            float r = UnityEngine.Random.Range(randomSizeMin, randomSizeMax);   // CHANGED
-            if (r < minRatio) r = minRatio;                     // CHANGED (clamp)
+            int usable = room.width;                 
+            float minRatio = (float)minRoomSize / usable;      
+            float r = UnityEngine.Random.Range(randomSizeMin, randomSizeMax);   
+            if (r < minRatio) r = minRatio;                    
 
-            int small = Mathf.RoundToInt(usable * r);           // CHANGED
-            small = Mathf.Clamp(small, minRoomSize, usable - minRoomSize); // safety
-            int big = usable - small;                           // CHANGED
+            int small = Mathf.RoundToInt(usable * r);          
+            small = Mathf.Clamp(small, minRoomSize, usable - minRoomSize); 
+            int big = usable - small;                           
 
-            bool aIsBig = RandomBool();                         // CHANGED
+            bool aIsBig = RandomBool();                        
 
-            int aWidth = aIsBig ? big : small;                  // CHANGED
-            int bWidth = usable - aWidth;                       // CHANGED (ensures sum is exact)
+            int aWidth = aIsBig ? big : small;                 
+            int bWidth = usable - aWidth;                       
 
             roomA = new RectInt(room.x, room.y, aWidth, room.height);
-            roomB = new RectInt(room.xMax - bWidth, room.y, bWidth, room.height); // CHANGED (centered wall)
+            roomB = new RectInt(room.xMax - bWidth, room.y, bWidth, room.height); 
 
             return true;
         }
@@ -208,22 +184,22 @@ public class DungeonGeneratorScript : MonoBehaviour
             // Y
             if (room.height < minRoomSize * 2) return false;
 
-            int usable = room.height;                // CHANGED
-            float minRatio = (float)minRoomSize / usable;       // CHANGED
-            float r = UnityEngine.Random.Range(randomSizeMin, randomSizeMax);   // CHANGED
-            if (r < minRatio) r = minRatio;                     // CHANGED
+            int usable = room.height;               
+            float minRatio = (float)minRoomSize / usable;      
+            float r = UnityEngine.Random.Range(randomSizeMin, randomSizeMax);  
+            if (r < minRatio) r = minRatio;                    
 
-            int small = Mathf.RoundToInt(usable * r);           // CHANGED
-            small = Mathf.Clamp(small, minRoomSize, usable - minRoomSize); // safety
-            int big = usable - small;                           // CHANGED
+            int small = Mathf.RoundToInt(usable * r);          
+            small = Mathf.Clamp(small, minRoomSize, usable - minRoomSize); 
+            int big = usable - small;                         
 
-            bool aIsBig = RandomBool();                         // CHANGED
+            bool aIsBig = RandomBool();                        
 
-            int aHeight = aIsBig ? big : small;                 // CHANGED
-            int bHeight = usable - aHeight;                     // CHANGED
+            int aHeight = aIsBig ? big : small;              
+            int bHeight = usable - aHeight;                  
 
             roomA = new RectInt(room.x, room.y, room.width, aHeight);
-            roomB = new RectInt(room.x, room.yMax - bHeight, room.width, bHeight); // CHANGED (centered wall)
+            roomB = new RectInt(room.x, room.yMax - bHeight, room.width, bHeight);
 
             return true;
         }
@@ -242,110 +218,4 @@ public class DungeonGeneratorScript : MonoBehaviour
         if (number % 2 == 0) return true;
         else return false;
     }
-
-    //private bool HasRoomOnBothSides(RectInt door)
-    //{
-    //    bool isVerticalDoor = door.width == wallSize;   // X-split door: spans wall in X, slides in Y
-    //    bool isHorizontalDoor = door.height == wallSize; // Y-split door: spans wall in Y, slides in X
-
-    //    if (isVerticalDoor)
-    //    {
-    //        // Door occupies the wall gap at x = door.x .. door.x+wallSize
-    //        // Room on left should touch door.x, room on right should touch door.x+wallSize
-    //        int leftTouchX = door.x;
-    //        int rightTouchX = door.x + wallSize;
-
-    //        bool hasLeft = false, hasRight = false;
-
-    //        for (int i = 0; i < roomsToDraw.Count; i++)
-    //        {
-    //            RectInt r = roomsToDraw[i];
-
-    //            // Overlap test on Y between room and door opening
-    //            bool overlapY = r.yMin < door.yMax && r.yMax > door.yMin;
-    //            if (!overlapY) continue;
-
-    //            if (r.xMax == leftTouchX) hasLeft = true;
-    //            if (r.xMin == rightTouchX) hasRight = true;
-
-    //            if (hasLeft && hasRight) return true;
-    //        }
-
-    //        return false;
-    //    }
-    //    else if (isHorizontalDoor)
-    //    {
-    //        // Door occupies wall gap at y = door.y .. door.y+wallSize
-    //        int bottomTouchY = door.y;
-    //        int topTouchY = door.y + wallSize;
-
-    //        bool hasBottom = false, hasTop = false;
-
-    //        for (int i = 0; i < roomsToDraw.Count; i++)
-    //        {
-    //            RectInt r = roomsToDraw[i];
-
-    //            bool overlapX = r.xMin < door.xMax && r.xMax > door.xMin;
-    //            if (!overlapX) continue;
-
-    //            if (r.yMax == bottomTouchY) hasBottom = true;
-    //            if (r.yMin == topTouchY) hasTop = true;
-
-    //            if (hasBottom && hasTop) return true;
-    //        }
-
-    //        return false;
-    //    }
-
-    //    // Unknown door orientation -> treat as invalid
-    //    return false;
-    //}
-
-    //private void FixDoorsAfterSplits()
-    //{
-    //    for (int i = DoorsToDraw.Count - 1; i >= 0; i--)
-    //    {
-    //        RectInt d = DoorsToDraw[i];
-
-    //        if (HasRoomOnBothSides(d))
-    //            continue;
-
-    //        // Try nudging along the "sliding" axis
-    //        RectInt dPlus = d;
-    //        RectInt dMinus = d;
-
-    //        if (d.width == wallSize)
-    //        {
-    //            // slides along Y
-    //            dPlus.y += doorSize;
-    //            dMinus.y -= doorSize;
-    //        }
-    //        else if (d.height == wallSize)
-    //        {
-    //            // slides along X
-    //            dPlus.x += doorSize;
-    //            dMinus.x -= doorSize;
-    //        }
-    //        else
-    //        {
-    //            DoorsToDraw.RemoveAt(i);
-    //            continue;
-    //        }
-
-    //        if (HasRoomOnBothSides(dPlus))
-    //        {
-    //            DoorsToDraw[i] = dPlus;
-    //            continue;
-    //        }
-
-    //        if (HasRoomOnBothSides(dMinus))
-    //        {
-    //            DoorsToDraw[i] = dMinus;
-    //            continue;
-    //        }
-
-    //        // Still bad -> delete it
-    //        DoorsToDraw.RemoveAt(i);
-    //    }
-    //}
 }
